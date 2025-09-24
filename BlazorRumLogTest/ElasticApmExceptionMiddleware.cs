2e05d8da1563b0aaa9a4aecdd25b51fc1ed29730
kjun <keisoft.kjun@gmail.com>
2025-09-24 23:07:21 +0900

    초기 커밋.

---
+++ BlazorRumLogTest/ElasticApmExceptionMiddleware.cs
@@ -0,0 +1,40 @@
+﻿using Elastic.Apm;
+using Elastic.Apm.Api;
+
+namespace BlazorRumLogTest
+{
+    public class ElasticApmExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ElasticApmExceptionMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                // Exception을 Elastic APM에 자동으로 보고
+                if (Agent.IsConfigured)
+                {
+                    Agent.Tracer.CaptureException(ex, labels: new Dictionary<string, Label>
+                {
+                    { "http_method", context.Request.Method },
+                    { "http_path", context.Request.Path.ToString() },
+                    { "http_query", context.Request.QueryString.ToString() },
+                    { "user_agent", context.Request.Headers.UserAgent.ToString() }
+                });
+                }
+
+                throw;
+            }
+        }
+    }
+
+}
---
+++ BlazorRumLogTest/Program.cs
@@ -0,0 +1,43 @@
+using BlazorRumLogTest.Components;
+
+namespace BlazorRumLogTest;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddRazorComponents()
+            .AddInteractiveServerComponents();
+
+        // Elastic APM 서비스 등록
+        builder.Services.AddElasticApm();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+
+        // Elastic APM 미들웨어 추가 (최신 방법)
+        //app.UseElasticApm();
+
+        app.UseMiddleware<ElasticApmExceptionMiddleware>();
+
+        app.UseHttpsRedirection();
+
+        app.UseAntiforgery();
+
+        app.MapStaticAssets();
+        app.MapRazorComponents<App>()
+            .AddInteractiveServerRenderMode();
+
+        app.Run();
+    }
+}
